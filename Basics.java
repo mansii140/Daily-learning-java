89abc710407a70a6a2aa8745f6f1256618139036
Mansi raja pramr <parmarmansi140@gmail.com>
2026-06-06 18:44:45 +0530

    Basic java learning

--- Basics.java
+++ Basics.java
@@ -1,8 +1,133 @@
+// output
+
 // printing hello world
-class Basics{
-    public static void main(String args [] ){
-        System.out.println("Hello world with java");
-        System.out.print("Hello world with java\nHello world with java\n");
-        System.out.print("Hello world with java");
+// class Basics{
+//     public static void main(String args [] ){
+//         System.out.println("Hello world with java");
+//         System.out.print("Hello world with java\nHello world with java\n");
+//         System.out.print("Hello world with java");
+//     }
+// }
+
+// printing stars
+// public class Basics{
+//     public static void main(String args []){
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.print("****");
+
+//     }
+// }
+
+// variables 
+
+// public class Basics{
+//     public static void main(String args []) {
+//         int a = 20;
+//         int b = 15;
+//         int sum = a + b;
+        // System.out.print(sum);
+
+        // int diff = a - b;
+        // System.out.print(diff);
+
+        // int mul = a * b;
+        // System.out.print(mul);
+
+
+
+//     }
+// }
+
+// input in java
+
+import java.util.*;
+
+// public class Basics{
+//     public static void main(String args []){
+//         Scanner sc  = new Scanner (System.in);
+//         // String name = sc.next();
+//         String name = sc.nextLine();
+//         // nextInt()
+//         // nextFloat()
+//         System.out.print(name);
+//     }
+// }
+
+
+// public class Basics {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner (System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a + b;
+//         System.out.print(sum);
+//     }
+// }
+
+
+// Questions 
+// 1.
+// public class Basics{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner (System.in);
+//         System.out.print("Enter your first no:");
+//         int A = sc.nextInt();
+//         System.out.print("Enter your second no:");
+//         int B = sc.nextInt();
+//         System.out.print("Enter your third no:");
+//         int C = sc.nextInt();
+//         int N = 3;
+//         float avg = (A + B + C) / N;
+//         System.out.print("The average of no:" + avg);
+//     }
+// }
+
+// 2.
+// public class Basics{
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter you side:");
+//         int side = sc.nextInt();
+//         int square = side * side ;
+//         System.out.print("Area of square is:" + square);
+//     } 
+// }
+
+// 3.
+// public class Basics{
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter cost of pencil:");
+//         float pencil = sc.nextFloat();
+//         System.out.print("Enter cost of pen:");
+//         float pen = sc.nextFloat();
+//         System.out.print("Enter cost of eraser:");
+//         float eraser = sc.nextFloat();
+//         float totalcost = pencil + pen + eraser ;
+//         System.out.print("Total cost of the item :" + totalcost);
+
+//         float gsttax = totalcost * 0.18f;
+//         float finalbill = totalcost + gsttax;
+
+//         System.out.println("18% gst tax: " + gsttax);
+//         System.out.print("final bill(including gst):" + finalbill);
+//     }
+// }
+
+// operators
+public class Basics{
+    public static void main(String args[]){
+        int a = 20;
+        // int b = ++a;
+        // int b = a++;
+        int b = --a;
+        // int sum = a+b;
+        System.out.println(a);
+        System.out.print(b);
+
     }
+
 }
+
---
+++ Conditions.java
@@ -0,0 +1,33 @@
+// import java.util.*;
+
+
+
+// if else 
+
+// public class Conditions{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner (System.in);
+//         int age = sc.nextInt();
+
+//         if (age >= 18){
+//             System.out.print("adult");
+//         }else{
+//             System.out.print("not adult");
+
+//         }
+//     }
+// }
+
+// public class Conditions{
+//     public static void main(String args []){
+//         Scanner sc = new Scanner (System.in);
+//         int num = sc.nextInt();
+
+//         if (num % 2 == 0){
+//             System.out.print("Even no.");
+//         }else {
+//             System.out.print("Odd no.");
+//         }
+//     }
+// }
+
