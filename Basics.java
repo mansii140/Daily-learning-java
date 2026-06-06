2970d7d26528099a1a87933163dc7c643d449950
Mansi raja pramr <parmarmansi140@gmail.com>
2026-06-06 15:32:44 +0530

    first code

---
+++ Basics.java
@@ -0,0 +1,8 @@
+// printing hello world
+class Basics{
+    public static void main(String args [] ){
+        System.out.println("Hello world with java");
+        System.out.print("Hello world with java\nHello world with java\n");
+        System.out.print("Hello world with java");
+    }
+}
