d6982a635747fd5d98054ed101339cba6f22aead
Mansi raja pramr <parmarmansi140@gmail.com>
2026-07-19 16:52:15 +0530

    Added Functions.java

---
+++ Functions.java
@@ -0,0 +1,76 @@
+// import java.util.*;
+// public class Functions {
+//     public static void printMyName(String name){
+//         System.out.println(name);
+        // return;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.next();
+
+//         printMyName(name);
+//     }
+// }
+
+
+// 1. Make a function to add two numbers and return the sum. 
+// public class Functions {
+//     public static int calculateSum(int a, int b){
+//         int sum = a + b;
+//         return sum;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your first no :");
+//         int a = sc.nextInt();
+//         System.out.println("Enter your second no :");
+//         int b = sc.nextInt();
+//         int sum = calculateSum(a, b);
+//         System.out.println("sum of two no. is : " + sum);
+        
+//     }
+// }
+
+// 2. Make a function to multiply 2 numbers and return the product
+// public class Functions {
+//     public static int calculateproduct(int a, int b ){
+//           int product = a * b;
+//           return product;
+
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your first no.");
+//         int a = sc.nextInt();
+//         System.out.println("Enter your second no.");
+//         int b = sc.nextInt();
+//         int product = calculateproduct(a, b);
+//         System.out.println("Product of two no is : " + product);
+//     }
+// }
+
+// 3.Print factorial 
+// public class Functions {
+    
+//     public static void printFactorial(int n) {
+//         if (n < 0){
+//         System.out.println("Invalid number");
+//         return;
+//     }
+//         int factorial = 1;
+//         for (int i = n; i>=1; i--){
+//             factorial = factorial * i;
+//         }
+//         System.out.println(factorial);
+//         return;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter no here:");
+//         int n = sc.nextInt();
+//         printFactorial(n);
+//     }
+// }
+
