5e8e696806c50ded479d9011d899618dc4c7596a
Mansi raja pramr <parmarmansi140@gmail.com>
2026-07-20 11:01:38 +0530

    Added Strings.java

---
+++ Strings.java
@@ -0,0 +1,57 @@
+// import java.util.*;
+
+// public class Strings {
+//     public static void main(String[] args) {
+//         String firstName = "Mansi";
+//         String lastName = "Parmar";
+//         String fullName = firstName + "@" + lastName;
+
+//         System.out.println(fullName.length());
+
+//         for (int i = 0; i < fullName.length(); i++) {
+//             System.out.println(fullName.charAt(i));
+//         }
+//     }
+// }
+
+// public class Strings {
+
+//     public static void main(String[] args) {
+//         String name1 = "Abhay";
+//         String name2 = "Abhay";
+
+//         if (name1.compareTo(name2) == 0) {
+//             System.out.println("Strings are equal");
+//         } else {
+//             System.out.println("Strings are not equal");
+
+//         }
+//     }
+// }
+
+// public class Strings {
+
+//     public static void main(String[] args) {
+//         StringBuilder sb = new StringBuilder("Abhay");
+//         System.out.println(sb);
+//         System.out.println(sb.charAt(0));
+//         System.out.println(sb.charAt(1));
+//         System.out.println(sb.charAt(2));
+//         System.out.println(sb.charAt(3));
+//         System.out.println(sb.charAt(4));
+
+//         sb.setCharAt(0, 'M');
+//         System.out.println(sb);
+
+//         sb.insert(0, 'A');
+//         System.out.println(sb);
+
+//         sb.insert(2, 'b');
+//         System.out.println(sb);
+
+//         sb.delete(2, 3);
+//         System.out.println(sb);
+
+//     }
+// }
+
