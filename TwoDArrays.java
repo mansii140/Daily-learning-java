087a7497237b6526a9bebb24a5a018fb15c2bf2b
Mansi raja pramr <parmarmansi140@gmail.com>
2026-07-20 10:58:04 +0530

    Added TwoDArray

---
+++ TwoDArrays.java
@@ -0,0 +1,37 @@
+// import java.util.Scanner;
+
+// public class TwoDArrays {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in); 
+
+//         System.out.println("Enter rows no:");
+//         int rows = sc.nextInt();
+
+//         System.out.println("Enter columns no:");
+//         int cols = sc.nextInt();
+
+//         System.out.println("Enter the matrix elements:");
+//         int[][] numbers = new int[rows][cols];
+
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+//                 numbers[i][j] = sc.nextInt();
+//             }
+//         }
+        
+//         System.out.println("Enter your x here:");
+//         int x = sc.nextInt();    
+
+//         // System.out.println("Your 2D Array:");
+
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+//                 if (numbers[i][j] == x) {
+//                     System.out.println("x found at location :(" + i + "," + j +")");
+                    
+//                 }
+//             }
+//         }
+
+//     }
+// }
